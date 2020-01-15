21ee3adf36deba73439f0bc973e136f8deac9f64
Malcolm Jones <bossjones@theblacktonystark.com>
2020-01-14 20:16:29 -0500

    4: mongodb

--- linkedin_learning/full_stack_web_dev_with_flask/application/__init__.py
+++ linkedin_learning/full_stack_web_dev_with_flask/application/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask_mongoengine import MongoEngine
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db = MongoEngine()
+db.init_app(app)
 
 # We don't want to have circular dependencies so call application after we've called app.
 
--- linkedin_learning/full_stack_web_dev_with_flask/config.py
+++ linkedin_learning/full_stack_web_dev_with_flask/config.py
@@ -1,5 +1,11 @@
 import os
 
-
 class Config(object):
     SECRET_KY = os.environ.get("SECRET_KEY") or "secret_string"
+
+    MONGODB_SETTINGS = {'db' : 'UTA_Enrollment'}
+
+    MONGODB_SETTINGS = {
+        'db': 'UTA_Enrollment',
+        'host': 'mongodb://172.16.2.234/UTA_Enrollment'
+    }
