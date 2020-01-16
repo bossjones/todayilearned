96523d87f2e5bd7e3fb89cf6d0827b9bf0fe440c
Malcolm Jones <bossjones@theblacktonystark.com>
2020-01-16 15:55:57 -0500

    5: creating mongodb aggregation pipeline

--- linkedin_learning/full_stack_web_dev_with_flask/application/routes.py
+++ linkedin_learning/full_stack_web_dev_with_flask/application/routes.py
@@ -266,42 +266,38 @@ def enrollment():
             Enrollment(user_id=user_id, courseID=courseID)
             flash(f"Youenrolled in {courseTitle}!", "success")
 
-    classes = list( User.objects.aggregate(*[
-            {
-                '$lookup': {
-                    'from': 'enrollment',
-                    'localField': 'user_id',
-                    'foreignField': 'user_id',
-                    'as': 'r1'
-                }
-            }, {
-                '$unwind': {
-                    'path': '$r1',
-                    'includeArrayIndex': 'r1_id',
-                    'preserveNullAndEmptyArrays': False
-                }
-            }, {
-                '$lookup': {
-                    'from': 'course',
-                    'localField': 'r1.courseID',
-                    'foreignField': 'courseID',
-                    'as': 'r2'
-                }
-            }, {
-                '$unwind': {
-                    'path': '$r2',
-                    'preserveNullAndEmptyArrays': False
-                }
-            }, {
-                '$match': {
-                    'user_id': user_id
-                }
-            }, {
-                '$sort': {
-                    'courseID': 1
-                }
-            }
-        ]))
+    classes = list(
+        User.objects.aggregate(
+            *[
+                {
+                    "$lookup": {
+                        "from": "enrollment",
+                        "localField": "user_id",
+                        "foreignField": "user_id",
+                        "as": "r1",
+                    }
+                },
+                {
+                    "$unwind": {
+                        "path": "$r1",
+                        "includeArrayIndex": "r1_id",
+                        "preserveNullAndEmptyArrays": False,
+                    }
+                },
+                {
+                    "$lookup": {
+                        "from": "course",
+                        "localField": "r1.courseID",
+                        "foreignField": "courseID",
+                        "as": "r2",
+                    }
+                },
+                {"$unwind": {"path": "$r2", "preserveNullAndEmptyArrays": False}},
+                {"$match": {"user_id": user_id}},
+                {"$sort": {"courseID": 1}},
+            ]
+        )
+    )
 
     return render_template("enrollment.html", enrollment=True, classes=classes)
 
