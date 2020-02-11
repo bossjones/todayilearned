09ffba4dab88071a8046fffe560400f65bafc5df
Malcolm Jones <bossjones@theblacktonystark.com>
2020-02-10 23:07:56 -0500

    WIP: logging stuff setup

--- logging_learning/boss_logging/config.py
+++ logging_learning/boss_logging/config.py
@@ -1,5 +1,5 @@
 import os
-from application.utils.logs import RequestIdFilter
+# from application.utils.logs import RequestIdFilter
 
 
 class Config(object):
@@ -14,43 +14,43 @@ class Config(object):
         "host": "mongodb://172.16.2.234/UTA_Enrollment",
     }
 
-    LOGGING = {
-        "version": 1,
-        "disable_existing_loggers": False,
-        "filters": {"request_id": {"()": RequestIdFilter,},},
-        "formatters": {
-            "json": {
-                "format": "%(asctime)s %(levelname)s %(name)s %(module)s %(funcName)s %(filename)s %(lineno)s %(request_id)s %(message)s",
-                "class": "pythonjsonlogger.jsonlogger.JsonFormatter",
-            },
-        },
-        "handlers": {
-            "console_message": {
-                "class": "logging.StreamHandler",
-                "level": "NOTSET",
-                "formatter": "json",
-                "filters": ["request_id"],
-            },
-        },
-        "loggers": {
-            "tests": {
-                "handlers": ["console_message"],
-                "propagate": False,
-                "level": "DEBUG",
-            },
-            "flask": {
-                "handlers": ["console_message"],
-                "propagate": False,
-                "level": os.environ.get("LOG_LEVEL", "WARNING"),
-            },
-            "werkzeug": {
-                "handlers": ["console_message"],
-                "propagate": False,
-                "level": os.environ.get("LOG_LEVEL", "WARNING"),
-            },
-        },
-        "root": {
-            "handlers": ["console_message"],
-            "level": os.environ.get("LOG_LEVEL", "WARNING"),
-        },
-    }
+    # LOGGING = {
+    #     "version": 1,
+    #     "disable_existing_loggers": False,
+    #     "filters": {"request_id": {"()": RequestIdFilter,},},
+    #     "formatters": {
+    #         "json": {
+    #             "format": "%(asctime)s %(levelname)s %(name)s %(module)s %(funcName)s %(filename)s %(lineno)s %(request_id)s %(message)s",
+    #             "class": "pythonjsonlogger.jsonlogger.JsonFormatter",
+    #         },
+    #     },
+    #     "handlers": {
+    #         "console_message": {
+    #             "class": "logging.StreamHandler",
+    #             "level": "NOTSET",
+    #             "formatter": "json",
+    #             "filters": ["request_id"],
+    #         },
+    #     },
+    #     "loggers": {
+    #         "tests": {
+    #             "handlers": ["console_message"],
+    #             "propagate": False,
+    #             "level": "DEBUG",
+    #         },
+    #         "flask": {
+    #             "handlers": ["console_message"],
+    #             "propagate": False,
+    #             "level": os.environ.get("LOG_LEVEL", "WARNING"),
+    #         },
+    #         # "werkzeug": {
+    #         #     "handlers": ["console_message"],
+    #         #     "propagate": False,
+    #         #     "level": os.environ.get("LOG_LEVEL", "WARNING"),
+    #         # },
+    #     },
+    #     "root": {
+    #         "handlers": ["console_message"],
+    #         "level": os.environ.get("LOG_LEVEL", "WARNING"),
+    #     },
+    # }
