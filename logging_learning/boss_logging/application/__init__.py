fb174d524db07ac9138315285b9f575295c6e5c7
Malcolm Jones <bossjones@theblacktonystark.com>
2020-02-10 22:44:48 -0500

    WIP: logging stuff setup

--- logging_learning/boss_logging/application/__init__.py
+++ logging_learning/boss_logging/application/__init__.py
@@ -1,13 +1,24 @@
+import logging
+from logging.config import dictConfig
 from flask import Flask
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_mongoengine import MongoEngine
 from config import Config
 from flask_restplus import Api
 
+logger = logging.getLogger(__name__)
+
 api = Api()
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+dictConfig(app.config.LOGGING)
+logger.info("Logging configured",
+            extra=dict(
+                root_logger_level=logging.getLogger().getEffectiveLevel(),
+                logger_level=logger.getEffectiveLevel()
+            ))
 
 db = MongoEngine()
 db.init_app(app)
---
+++ logging_learning/boss_logging/application/utils/logs.py
@@ -0,0 +1,42 @@
+from flask import request
+import logging
+import uuid
+import flask
+
+
+class RequestFormatter(logging.Formatter):
+    def format(self, record):
+        if request:
+            record.url = request.url
+            record.remote_addr = request.remote_addr
+        else:
+            record.url = None
+            record.remote_addr = None
+        return super().format(record)
+
+
+def generate_request_id(original_id=''):
+    new_id = uuid.uuid4()
+
+    if original_id:
+        new_id = "{},{}".format(original_id, new_id)
+
+    return new_id
+
+
+def request_id():
+    if getattr(flask.g, 'request_id', None):
+        return flask.g.request_id
+
+    headers = flask.request.headers
+    original_request_id = headers.get("X-Request-Id")
+    new_uuid = generate_request_id(original_request_id)
+    flask.g.request_id = new_uuid
+
+    return new_uuid
+
+
+class RequestIdFilter(logging.Filter):
+    def filter(self, record):
+        record.request_id = request_id() if flask.has_request_context() else ''
+        return True
